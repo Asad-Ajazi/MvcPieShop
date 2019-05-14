bf3c5ae64fbdd13433dc01acf9e50277aacdbb4d
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-14 23:05:09 +0100

    created database using ef core migrations, edit program.cs to get access to DI container to use static method to populate db if empty.

--- MvcPieShop/Program.cs
+++ MvcPieShop/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MvcPieShop.Models;
 
 namespace MvcPieShop
 {
@@ -14,7 +16,26 @@ namespace MvcPieShop
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
+                    // any logging etc.
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
