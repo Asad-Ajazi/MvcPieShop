14f2b308966a325e2faa5603d95ab51c161df517
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-14 22:37:27 +0100

    Added support for ef core - dbcontext

---
+++ MvcPieShop/Models/PieRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContex)
+        {
+            _appDbContext = appDbContex;
+        }
+
+
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDbContext.Pies;
+        }
+
+        public Pie GetById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
