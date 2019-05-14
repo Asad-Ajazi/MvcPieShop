a6bfb041853e1b6623707cc5e95b77e95cf6cb62
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-14 20:36:48 +0100

    Added Pie,IPieRepository and registered with dependency injection system. returns list of pies/by id.

---
+++ MvcPieShop/Models/IPieRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetById(int pieId);
+    }
+}
--- MvcPieShop/Startup.cs
+++ MvcPieShop/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using MvcPieShop.Models;
 
 namespace MvcPieShop
 {
@@ -15,6 +16,8 @@ namespace MvcPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // temp, when asked for ipie, give mockpie.
+            services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddMvc();
         }
 
