f816e6d9d5fdde70b10b15e1dcb262088c295c35
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-05-14 21:25:49 +0100

    Added home controller and view files/folder structure for mvc.

---
+++ MvcPieShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcPieShop.Models;
+
+namespace MvcPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "ViewbagTitlePieShop";
+            // get all pies and order by name then pass to view.
+            var pies = _pieRepository.GetAllPies().OrderBy(p=>p.Name);
+
+            return View(pies);
+        }
+    }
+}
